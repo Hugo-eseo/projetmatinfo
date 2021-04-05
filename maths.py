cf37e0b6adf6815130e5bf6af658e24aaa2a6959
Weezy-San <61516506+Weezy-San@users.noreply.github.com>
2021-04-05 22:00:36 +0200

    Update maths.py
    ajout de la reconnaissance lorsque les droites sont parallèles.

--- maths.py
+++ maths.py
@@ -10,12 +10,13 @@ def barycentre(A1, A2, B1, B2):
     part2 = (det(B2, B1, A1) * A2[0], det(B2, B1, A1) * A2[1])
     numerateur = (part1[0] + part2[0], part1[1] + part2[1])
     denominateur = det(B1, B2, A2) + det(B2, B1, A1)
+    if denominateur == 0: return None
     I = (numerateur[0] / denominateur, numerateur[1] / denominateur)
     return I
 
 if __name__ == '__main__':
     A1 = (-1, -1)
     A2 = (3, 1)
-    B1 = (0, 1)
+    B1 = (6, 0)
     B2 = (2, -2)
     print(barycentre(A1, A2, B1, B2))   # ça fonctionne j'ai dessiné sur papier pour comparer
