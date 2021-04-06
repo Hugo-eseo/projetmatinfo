dac2cc9a06944430b1c99a7ed8c140c091f9b0f3
Weezy-San <61516506+Weezy-San@users.noreply.github.com>
2021-04-06 21:30:04 +0200

    Update create_polygon_by_clic.py
    ajout d'une fonction determinant si le gardien est dans le polygone, si non, le gardien n'est pas affiché

--- create_polygon_by_clic.py
+++ create_polygon_by_clic.py
@@ -1,6 +1,37 @@
 import tkinter as tk
 from maths import barycentre, det, distance_two_points
 
+def point_in_polygon(point, segments_list):
+    side_x = (0, point[1])
+    side_y = (point[0], 0)
+    cpt_x, cpt_y = 0, 0
+    intersections_x_list = []
+    intersections_y_list = []
+    for segment in segments_list:
+        intersection_x = barycentre(side_x, point, segment[0], segment[1])
+        intersection_y = barycentre(side_y, point, segment[0], segment[1])
+        if intersection_x is not None:
+            if (intersection_x[0] > segment[0][0] and intersection_x[0] < segment[1][0]) or (intersection_x[0] < segment[0][0] and intersection_x[0] > segment[1][0]):
+                intersections_x_list.append(intersection_x)
+        if intersection_y is not None:
+            if (intersection_y[1] > segment[0][1] and intersection_y[1] < segment[1][1]) or (intersection_y[1] < segment[0][1] and intersection_y[1] > segment[1][1]):
+                intersections_y_list.append(intersection_y)
+    if len(intersections_x_list):
+        intersections_x_list.sort(key=lambda tupple: tupple[0])
+        for intersection in intersections_x_list:
+            if point[0] > intersection[0]:
+                cpt_x += 1 
+    if len(intersections_y_list):
+        intersections_y_list.sort(key=lambda tupple: tupple[1])
+        for intersection in intersections_y_list:
+            if point[1] > intersection[1]:
+                cpt_y += 1
+    if cpt_x % 2 != 0 and cpt_y % 2 != 0:
+        return True
+    else: 
+        return False
+    
+
 def polygon_by_clic(event, cnv):
     global coords_list
     x, y = event.x, event.y
@@ -22,7 +53,7 @@ def guardian_by_clic(event, cnv, segments_list):
     cnv.delete('guardian')
     cnv.delete('light')
     x0, y0 = event.x, event.y
-    cnv.create_oval(x0-5, y0-5, x0+5, y0+5, fill='black', tag='guardian')
+    is_in_polygon = point_in_polygon((x0, y0), segments_list)
     def light(x0, y0, side, segments_list):
         # side is a point (tuple) of 2
         for segment in segments_list:
@@ -36,12 +67,11 @@ def guardian_by_clic(event, cnv, segments_list):
                     cnv.create_line(A1[0], A1[1], intersection[0], intersection[1], tag='light')
                 elif (intersection[1] > B1[1] and intersection[1] < B2[1]) or (intersection[1] < B1[1] and intersection[1] > B2[1]):
                     cnv.create_line(A1[0], A1[1], intersection[0], intersection[1], tag='light')
-
-    for width in range(0, 601, 100):
-        for height in range(0, 601, 100):
-            light(x0, y0, (width, height), segments_list)
-        
-
+    if is_in_polygon:
+        cnv.create_oval(x0-5, y0-5, x0+5, y0+5, fill='black', tag='guardian')
+        for width in range(0, 601, 100):
+            for height in range(0, 601, 100):
+                light(x0, y0, (width, height), segments_list)
 
 def delete_points(cnv):
     global coords_list
