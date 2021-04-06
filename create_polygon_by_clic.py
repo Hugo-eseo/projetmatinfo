2d462e48b6517d14030c4cf9cbd8cc2c4ea298a5
Weezy-San <61516506+Weezy-San@users.noreply.github.com>
2021-04-06 12:07:55 +0200

    Update create_polygon_by_clic.py
    avancée, correction de bugs sur les boutons ainsi que projection des rayons sur n-1 cotés (manque donc un coté), cependant un bug à deja été detecté, les rayons traversent les murs pour aller sur le mur le plus lointain.

--- create_polygon_by_clic.py
+++ create_polygon_by_clic.py
@@ -9,13 +9,14 @@ def polygon_by_clic(event, cnv):
 
 def draw(cnv):
     global coords_list, segments_list
-    cnv.create_polygon(coords_list, fill='grey', outline='black')
-    segments_list.clear()
-    for i in range(len(coords_list)-1):
-        segments_list.append((coords_list[i], coords_list[i+1]))
-    segments_list.append((coords_list[-1], coords_list[0]))
-    coords_list.clear()
-    cnv.delete('point')
+    if len(coords_list):
+        cnv.create_polygon(coords_list, fill='grey', outline='black')
+        segments_list.clear()
+        for i in range(len(coords_list)-1):
+            segments_list.append((coords_list[i], coords_list[i+1]))
+        segments_list.append((coords_list[-1], coords_list[0]))
+        coords_list.clear()
+        cnv.delete('point')
 
 def guardian_by_clic(event, cnv, segments_list):
     cnv.delete('guardian')
@@ -23,18 +24,31 @@ def guardian_by_clic(event, cnv, segments_list):
     x0, y0 = event.x, event.y
     cnv.create_oval(x0-5, y0-5, x0+5, y0+5, fill='black', tag='guardian')
     def light(x0, y0, segments_list):
-        for i in range(0, 400, 30):
-            for j in range(0, 600, 30):
+        for i in range(0, 400, 100):
+            for j in range(0, 600, 100):
                 A1 = (x0, y0)
-                A2 = (i, j) 
-                B1 = segments_list[0][0]
-                B2 = segments_list[0][1]
+                A2 = (i, j)
+                B1 = segments_list[0]
+                B2 = segments_list[1]
                 intersection = barycentre(A1, A2, B1, B2) 
                 if intersection is not None:
                     if det(B1, B2, intersection) == 0:
-                        cnv.create_line(A1, A2, intersection[0], intersection[1], tag='light')
-    light(x0, y0, segments_list)
+                        if intersection[0] > B1[0] and intersection[0] < B2[0]:
+                            cnv.create_line(A1[0], A1[1], intersection[0], intersection[1], tag='light')
+                        elif intersection[1] > B1[1] and intersection[1] < B2[1]:
+                            cnv.create_line(A1[0], A1[1], intersection[0], intersection[1], tag='light')
+    for k in range(len(segments_list)):
+        light(x0, y0, segments_list[k])
 
+def delete_points(cnv):
+    global coords_list
+    cnv.delete('point')
+    coords_list.clear()
+
+def delete_all(cnv):
+    global coords_list
+    cnv.delete('all')
+    coords_list.clear()
 
 if __name__ == '__main__':
     # few variables
@@ -47,9 +61,9 @@ if __name__ == '__main__':
     cnv.pack()
     draw_button = tk.Button(wnd, command=lambda: draw(cnv), text='Draw polygon !')
     draw_button.pack(side=tk.BOTTOM)
-    delete_point_button = tk.Button(wnd, command=lambda: cnv.delete('point'), text='Delete all points')
+    delete_point_button = tk.Button(wnd, command=lambda: delete_points(cnv), text='Delete all points')
     delete_point_button.pack(side=tk.BOTTOM)
-    delete_all_button = tk.Button(wnd, command=lambda: cnv.delete('all'), text='Delete all')
+    delete_all_button = tk.Button(wnd, command=lambda: delete_all(cnv), text='Delete all')
     delete_all_button.pack(side=tk.BOTTOM)
     cnv.bind('<1>', lambda event, cnv=cnv: polygon_by_clic(event, cnv))
     cnv.bind('<2>', lambda event, cnv=cnv: guardian_by_clic(event, cnv, segments_list))
