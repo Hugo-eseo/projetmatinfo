4269152038e9fb094ea5b956672c459fd7d7886b
Hugo-eseo <70439592+Hugo-eseo@users.noreply.github.com>
2021-04-26 12:13:21 +0200

    small advance

--- polygon_eclairage.py
+++ polygon_eclairage.py
@@ -5,7 +5,7 @@ Created on Fri Apr 23 21:44:36 2021
 @author: hugob
 """
 
-from shared import point_classe, segment_classe
+from shared import point_classe, segment_classe, intersection_segments
 
 # Taille des point affichés sur le canvas
 size = 4
@@ -40,8 +40,25 @@ def polygon_eclairage(start_point, polygon, canvas, mode_demo=False):
     B = point_classe(polygon[0][0], polygon[0][1])
     liste_segments_polygon.append(segment_classe(A, B))
 
-    # Le point 0 dans tout l'algorithme correspond au point
-    # dont nous souhaitons savoir si il est dans le poylgon
+    # Création d'une liste contenant les sommets du polygon
+    liste_sommets_polygon = polygon    
+
+    # Le point O est le point où l'on souhaite connaître le polygon d'éclairage
     O = point_classe(start_point[0], start_point[1])
+
+    # Suppression de la précédente source lumineuse
+    canvas.delete('light')
+    # Affichage de la source lumineuse en jaune
+    canvas.create_oval(O.x-size, O.y-size, O.x+size, O.y+size, fill='white',
+                       tag='light')
+
+    # Liste des points d'intersections avec les sommets du polygon
+    liste_intersections = list()
+    
+    for sommet in liste_sommets_polygon():
+        # On cherche toutes les intersections avec les segments renseignés
+        for segment in liste_segments_polygon():
+            I = intersection_segments(segment_horizontal, segment)
+    
     
     
